faea8965255b3f29acf90f9cfd06dc1ca4b2347f
PetyaYakimova <petya_yakimova@abv.bg>
2024-05-08 08:48:27 +0300

    Add claims principle extension

---
+++ HouseRentingSystemFromFile/Infrastructure/ClaimsPrincipleExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace HouseRentingSystemFromFile.Infrastructure
+{
+    public static class ClaimsPrincipleExtensions
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+    }
+}
