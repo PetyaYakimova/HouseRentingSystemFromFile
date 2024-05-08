d1084e2b1c71576e34fa45f1475658ec097c859c
PetyaYakimova <petya_yakimova@abv.bg>
2024-05-08 08:48:47 +0300

    Update view and service models

--- HouseRentingSystemFromFile/Models/Agent/BecomeAgentFormModel.cs
+++ HouseRentingSystemFromFile/Models/Agent/BecomeAgentFormModel.cs
@@ -1,6 +1,14 @@
-﻿namespace HouseRentingSystemFromFile.Models.Agent
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystemFromFile.Data.DataConstants;
+
+namespace HouseRentingSystemFromFile.Models.Agent
 {
 	public class BecomeAgentFormModel
-	{
-	}
+    {
+        [Required]
+        [StringLength(PhoneNumberMaxLength, MinimumLength = PhoneNumberMinLength)]
+        [Display(Name = "Phone Number")]
+        [Phone]
+        public string PhoneNumber { get; set; } = null!;
+    }
 }
