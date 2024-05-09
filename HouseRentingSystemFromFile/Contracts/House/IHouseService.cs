d11ed80cb60f35eccf552c1b019c7e2d42484e71
PetyaYakimova <petya_yakimova@abv.bg>
2024-05-09 08:34:51 +0300

    add methods for house service

--- HouseRentingSystemFromFile/Contracts/House/IHouseService.cs
+++ HouseRentingSystemFromFile/Contracts/House/IHouseService.cs
@@ -1,9 +1,29 @@
-﻿using HouseRentingSystemFromFile.Models.House;
+﻿using HouseRentingSystemFromFile.Infrastructure;
+using HouseRentingSystemFromFile.Models.House;
 
 namespace HouseRentingSystemFromFile.Contracts.House
 {
 	public interface IHouseService
 	{
 		Task<IEnumerable<HouseIndexServiceModel>> LastThreeHouses();
-	}
+
+        Task<IEnumerable<HouseCategoryServiceModel>> AllCategories();
+
+        Task<bool> CategoryExists(int categoryId);
+
+        Task<int> Create(string title, string address, string description, string imageUrl, decimal price,
+            int categoryId, int agentId);
+
+        HouseQueryServiceModel All(string? category = null, 
+            string? searchTerm = null, 
+            HouseSorting sorting = HouseSorting.Newest, 
+            int currentPage = 1, 
+            int housesPerPage = 1);
+
+        Task<IEnumerable<string>> AllCategoriesNames();
+
+        Task<IEnumerable<HouseServiceModel>> AllHousesByAgentId(int agentId);
+
+        Task<IEnumerable<HouseServiceModel>> AllHousesByUserId(string userId);
+    }
 }
--- HouseRentingSystemFromFile/Services/House/HouseService.cs
+++ HouseRentingSystemFromFile/Services/House/HouseService.cs
@@ -1,30 +1,166 @@
 ﻿using HouseRentingSystemFromFile.Contracts.House;
 using HouseRentingSystemFromFile.Data;
+using HouseRentingSystemFromFile.Infrastructure;
 using HouseRentingSystemFromFile.Models.House;
+using Microsoft.EntityFrameworkCore;
 
 namespace HouseRentingSystemFromFile.Services.House
 {
-	public class HouseService : IHouseService
-	{
-		private readonly HouseRentingDbContext _data;
+    public class HouseService : IHouseService
+    {
+        private readonly HouseRentingDbContext _data;
 
-		public HouseService(HouseRentingDbContext data)
-		{
-			_data = data;
-		}
+        public HouseService(HouseRentingDbContext data)
+        {
+            _data = data;
+        }
 
-		public async Task<IEnumerable<HouseIndexServiceModel>> LastThreeHouses()
-		{
-			return _data
-				.Houses
-				.OrderByDescending(c => c.Id)
-				.Select(c => new HouseIndexServiceModel()
-				{
-					Id = c.Id,
-					Title = c.Title,
-					ImageUrl = c.ImageUrl
-				})
-				.Take(3);
-		}
-	}
+        public async Task<IEnumerable<HouseIndexServiceModel>> LastThreeHouses()
+        {
+            return _data
+                .Houses
+                .OrderByDescending(c => c.Id)
+                .Select(c => new HouseIndexServiceModel()
+                {
+                    Id = c.Id,
+                    Title = c.Title,
+                    ImageUrl = c.ImageUrl
+                })
+                .Take(3);
+        }
+
+        public async Task<IEnumerable<HouseCategoryServiceModel>> AllCategories()
+        {
+            return await _data.Categories
+                .Select(c => new HouseCategoryServiceModel()
+                {
+                    Id = c.Id,
+                    Name = c.Name
+                })
+                .ToListAsync();
+        }
+
+        public async Task<bool> CategoryExists(int categoryId)
+        {
+            return await _data.Categories.AnyAsync(c => c.Id == categoryId);
+        }
+
+        public async Task<int> Create(string title, string address, string description, string imageUrl, decimal price, int categoryId,
+            int agentId)
+        {
+            var house = new Data.Models.House()
+            {
+                Title = title,
+                Address = address,
+                Description = description,
+                ImageUrl = imageUrl,
+                PricePerMonth = price,
+                CategoryId = categoryId,
+                AgentId = agentId
+            };
+
+            await _data.Houses.AddAsync(house);
+            await _data.SaveChangesAsync();
+
+            return house.Id;
+        }
+
+        public HouseQueryServiceModel All(string? category = null, string? searchTerm = null,
+            HouseSorting sorting = HouseSorting.Newest, int currentPage = 1, int housesPerPage = 1)
+        {
+            var housesQuery = _data.Houses.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(category))
+            {
+                housesQuery = _data.Houses
+                    .Where(h => h.Category.Name == category);
+            }
+
+            if (!string.IsNullOrWhiteSpace(searchTerm))
+            {
+                housesQuery = housesQuery
+                    .Where(h =>
+                        h.Title.ToLower().Contains(searchTerm.ToLower()) ||
+                        h.Address.ToLower().Contains(searchTerm.ToLower()) ||
+                        h.Description.ToLower().Contains(searchTerm.ToLower()));
+            }
+
+            housesQuery = sorting switch
+            {
+                HouseSorting.Price => housesQuery
+                    .OrderBy(h => h.PricePerMonth),
+                HouseSorting.NotRentedFirst => housesQuery
+                    .OrderBy(h => h.RenterId != null)
+                    .ThenByDescending(h => h.Id),
+                _ => housesQuery.OrderByDescending(h => h.Id)
+            };
+
+            var houses = housesQuery
+                .Skip((currentPage - 1) * housesPerPage)
+                .Take(housesPerPage)
+                .Select(h => new HouseServiceModel()
+                {
+                    Id = h.Id,
+                    Title = h.Title,
+                    Address = h.Address,
+                    ImageUrl = h.ImageUrl,
+                    IsRented = h.RenterId != null,
+                    PricePerMonth = h.PricePerMonth
+                })
+                .ToList();
+
+            var totalHouses = housesQuery.Count();
+
+            return new HouseQueryServiceModel()
+            {
+                TotalHousesCount = totalHouses,
+                Houses = houses
+            };
+        }
+
+        public async Task<IEnumerable<string>> AllCategoriesNames()
+        {
+            return await _data.Categories
+                .Select(c => c.Name)
+                .Distinct()
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<HouseServiceModel>> AllHousesByAgentId(int agentId)
+        {
+            var houses = await _data
+                .Houses
+                .Where(h => h.AgentId == agentId)
+                .ToListAsync();
+
+            return ProjectToModel(houses);
+        }
+
+        public async Task<IEnumerable<HouseServiceModel>> AllHousesByUserId(string userId)
+        {
+            var houses = await _data
+                .Houses
+                .Where(h => h.RenterId == userId)
+                .ToListAsync();
+
+            return ProjectToModel(houses);
+        }
+
+        private List<HouseServiceModel> ProjectToModel(List<Data.Models.House> houses)
+        {
+            var resultHouses = houses
+                .Select(h => new HouseServiceModel()
+                {
+                    Id = h.Id,
+                    Title = h.Title,
+                    Address = h.Address,
+                    ImageUrl = h.ImageUrl,
+                    PricePerMonth = h.PricePerMonth,
+                    IsRented = h.RenterId != null
+                })
+                .ToList();
+
+            return resultHouses;
+        }
+    }
 }
