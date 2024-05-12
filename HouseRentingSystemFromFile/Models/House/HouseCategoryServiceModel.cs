a5d2545cd975a7322fef9d5d53d3e26b18c65de3
PetyaYakimova <petya_yakimova@abv.bg>
2024-05-12 09:18:34 +0300

    Add house category service model

---
+++ HouseRentingSystemFromFile/Models/House/HouseCategoryServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HouseRentingSystemFromFile.Models.House
+{
+    public class HouseCategoryServiceModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+    }
+}
