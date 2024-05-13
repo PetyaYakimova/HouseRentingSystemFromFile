81a5ff6e375cdcc5df9ee615e49c1918442e920e
PetyaYakimova <petya_yakimova@abv.bg>
2024-05-13 08:47:32 +0300

    Update house form model

--- HouseRentingSystemFromFile/Models/House/HouseFormModel.cs
+++ HouseRentingSystemFromFile/Models/House/HouseFormModel.cs
@@ -1,6 +1,35 @@
-﻿namespace HouseRentingSystemFromFile.Models.House
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystemFromFile.Data.DataConstants;
+
+namespace HouseRentingSystemFromFile.Models.House
 {
 	public class HouseFormModel
-	{
-	}
+    {
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(AddressMaxLength, MinimumLength = AddressMinLength)]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Image URL")]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [Range(0.00, MxPricePerMonth,
+            ErrorMessage = "Price Per Month must be a positive number and less than {2} leva.")]
+        [Display(Name = "Price Per Month")]
+        public decimal PricePerMonth { get; set; }
+
+        [Display(Name="Category")]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<HouseCategoryServiceModel> Categories { get; set; } = new List<HouseCategoryServiceModel>();
+    }
 }
