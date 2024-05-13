53913e25ebbd9b79e8fb8cbc273ee6adccd928aa
PetyaYakimova <petya_yakimova@abv.bg>
2024-05-13 08:47:52 +0300

    add house service model

---
+++ HouseRentingSystemFromFile/Models/House/HouseServiceModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HouseRentingSystemFromFile.Models.House
+{
+    public class HouseServiceModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+
+        [Display(Name = "image URL")]
+        public string ImageUrl { get; set; } = null!;
+
+        [Display(Name = "Price Per Month")]
+        public decimal PricePerMonth { get; set; }
+
+        [Display(Name = "Is Rented")]
+        public bool IsRented { get; set; }
+    }
+}
