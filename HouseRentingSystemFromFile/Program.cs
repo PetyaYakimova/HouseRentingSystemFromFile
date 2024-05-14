fab53b168647f156099063d8f718e3fb585f8589
PetyaYakimova <petya_yakimova@abv.bg>
2024-05-14 08:29:11 +0300

    Change exception handler in program class

--- HouseRentingSystemFromFile/Program.cs
+++ HouseRentingSystemFromFile/Program.cs
@@ -10,18 +10,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<HouseRentingDbContext>(options =>
-	options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
-	{
-		options.SignIn.RequireConfirmedAccount = false;
-		options.Password.RequireDigit = false;
-		options.Password.RequireLowercase = false;
-		options.Password.RequireUppercase = false;
-		options.Password.RequireNonAlphanumeric = false;
-	})
-	.AddEntityFrameworkStores<HouseRentingDbContext>();
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+    })
+    .AddEntityFrameworkStores<HouseRentingDbContext>();
 
 builder.Services.AddControllersWithViews();
 
@@ -30,16 +30,16 @@ builder.Services.AddTransient<IAgentService, AgentService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
 }
 else
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error/500");
+    app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
