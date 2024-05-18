0d29bc8645c1a2833f0102d353bb8c0a470b29f2
PetyaYakimova <petya_yakimova@abv.bg>
2024-05-18 09:20:22 +0300

    Add model extensions

---
+++ HouseRentingSystemFromFile/Infrastructure/ModelExtensions.cs
@@ -0,0 +1,19 @@
+﻿using HouseRentingSystemFromFile.Contracts;
+using System.Text.RegularExpressions;
+
+namespace HouseRentingSystemFromFile.Infrastructure
+{
+	public static class ModelExtensions
+	{
+		public static string GetInformation(this IHouseModel house)
+		{
+			return house.Title.Replace(" ", "-") + " " + GetAddress(house.Address);
+		}
+
+		private static string GetAddress(string address)
+		{
+			address = String.Join("-", address.Split(" ").Take(3));
+			return Regex.Replace(address, @"[^a-zA-Z0-9\-]", string.Empty);
+		}
+	}
+}
