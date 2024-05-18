c8c02991c36aed65163f2200b5732b2796bac7dc
PetyaYakimova <petya_yakimova@abv.bg>
2024-05-18 09:20:12 +0300

    Add statistic api controller

---
+++ HouseRentingSystemFromFile/Controllers/Api/StatisticApiController.cs
@@ -0,0 +1,24 @@
+﻿using HouseRentingSystemFromFile.Contracts.Statistic;
+using HouseRentingSystemFromFile.Models.Statistic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystemFromFile.Controllers.Api
+{
+	[ApiController]
+	[Route("api/statistic")]
+	public class StatisticApiController : ControllerBase
+	{
+		private readonly IStatisticService _statistics;
+
+		public StatisticApiController(IStatisticService statistics)
+		{
+			_statistics = statistics;
+		}
+
+		[HttpGet]
+		public async Task<StatisticServiceModel> GetStatistic()
+		{
+			return await _statistics.Total();
+		}
+	}
+}
