3fb3abaa9c5486e501e3d3828e43ac93f40bd7a3
PetyaYakimova <petya_yakimova@abv.bg>
2024-05-19 09:31:34 +0300

    Update the house service model

--- HouseRentingSystemFromFile/Models/House/HouseServiceModel.cs
+++ HouseRentingSystemFromFile/Models/House/HouseServiceModel.cs
@@ -1,8 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using HouseRentingSystemFromFile.Contracts;
+using System.ComponentModel.DataAnnotations;
 
 namespace HouseRentingSystemFromFile.Models.House
 {
-    public class HouseServiceModel
+    public class HouseServiceModel : IHouseModel
     {
         public int Id { get; set; }
 
