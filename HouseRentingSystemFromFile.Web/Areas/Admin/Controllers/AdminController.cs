7d123a950cdb0a0e7aa84f74b48394e4cddbf65d
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-10 11:02:42 +0300

    Add admin base controller

---
+++ HouseRentingSystemFromFile.Web/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static HouseRentingSystemFromFile.Data.Data.AdminConstants;
+
+namespace HouseRentingSystemFromFile.Web.Areas.Admin.Controllers
+{
+	[Area(AreaName)]
+	[Authorize(Roles = AdminRoleName)]
+	public class AdminController : Controller
+	{
+	}
+}
