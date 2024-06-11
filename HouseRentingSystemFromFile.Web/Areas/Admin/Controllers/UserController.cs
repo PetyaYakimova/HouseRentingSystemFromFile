dbce3e0fcb03ce389818a704b5fb3fabe2c2a350
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-11 09:08:06 +0300

    Add User controller for admins

---
+++ HouseRentingSystemFromFile.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using HouseRentingSystemFromFile.Core.Contracts.ApplicationUser;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystemFromFile.Web.Areas.Admin.Controllers
+{
+	public class UserController : AdminController
+	{
+		private readonly IApplicationUserService _users;
+
+		public UserController(IApplicationUserService users)
+		{
+			_users = users;
+		}
+
+		[Route("User/All")]
+		public async Task<IActionResult> All()
+		{
+			var users = await _users.All();
+			return View(users);
+		}
+	}
+}
