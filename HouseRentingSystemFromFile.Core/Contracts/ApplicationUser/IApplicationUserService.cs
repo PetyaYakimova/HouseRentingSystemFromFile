e27bfad3e326b9c9ad5dde42b334da5083f54f89
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-11 09:07:11 +0300

    Update user service with method to display all users

--- HouseRentingSystemFromFile.Core/Contracts/ApplicationUser/IApplicationUserService.cs
+++ HouseRentingSystemFromFile.Core/Contracts/ApplicationUser/IApplicationUserService.cs
@@ -1,7 +1,11 @@
-﻿namespace HouseRentingSystemFromFile.Core.Contracts.ApplicationUser
+﻿using HouseRentingSystemFromFile.Core.Models.User;
+
+namespace HouseRentingSystemFromFile.Core.Contracts.ApplicationUser
 {
     public interface IApplicationUserService
     {
         Task<string?> UserFullName(string userId);
+
+        Task<IEnumerable<UserServiceModel>> All();
     }
 }
--- HouseRentingSystemFromFile.Core/Services/ApplicationUser/ApplicationUserService.cs
+++ HouseRentingSystemFromFile.Core/Services/ApplicationUser/ApplicationUserService.cs
@@ -1,28 +1,57 @@
-﻿using HouseRentingSystemFromFile.Core.Contracts.ApplicationUser;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using HouseRentingSystemFromFile.Core.Contracts.ApplicationUser;
+using HouseRentingSystemFromFile.Core.Models.User;
 using HouseRentingSystemFromFile.Data.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace HouseRentingSystemFromFile.Core.Services.ApplicationUser
 {
-    public class ApplicationUserService : IApplicationUserService
-    {
-        private readonly HouseRentingDbContext _data;
+	public class ApplicationUserService : IApplicationUserService
+	{
+		private readonly HouseRentingDbContext _data;
+		private readonly IMapper _mapper;
 
-        public ApplicationUserService(HouseRentingDbContext data)
-        {
-            _data = data;
-        }
+		public ApplicationUserService(HouseRentingDbContext data, IMapper mapper)
+		{
+			_data = data;
+			_mapper = mapper;
+		}
 
-        public async Task<string?> UserFullName(string userId)
-        {
-            var user = await _data.Users.FindAsync(userId);
+		public async Task<IEnumerable<UserServiceModel>> All()
+		{
+			var allUsers = new List<UserServiceModel>();
 
-            if (string.IsNullOrEmpty(user.FirstName)
-                || string.IsNullOrEmpty(user.LastName))
-            {
-                return null;
-            }
+			var agents = await _data
+				.Agents
+				.Include(ag => ag.User)
+				.ProjectTo<UserServiceModel>(_mapper.ConfigurationProvider)
+				.ToListAsync();
 
-            return user.FirstName + " " + user.LastName;
-        }
-    }
+			allUsers.AddRange(agents);
+
+			var users = await _data
+				.Users
+				.Where(u => !_data.Agents.Any(ag => ag.UserId == u.Id))
+				.ProjectTo<UserServiceModel>(_mapper.ConfigurationProvider)
+				.ToListAsync();
+
+			allUsers.AddRange(users);
+
+			return allUsers;
+		}
+
+		public async Task<string?> UserFullName(string userId)
+		{
+			var user = await _data.Users.FindAsync(userId);
+
+			if (string.IsNullOrEmpty(user.FirstName)
+				|| string.IsNullOrEmpty(user.LastName))
+			{
+				return null;
+			}
+
+			return user.FirstName + " " + user.LastName;
+		}
+	}
 }
