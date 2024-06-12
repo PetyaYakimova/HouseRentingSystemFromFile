c8aa4936fc9c09c312a50488e51c9648023a0939
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-12 08:54:17 +0300

    Add caching constants

--- HouseRentingSystemFromFile.Data/Data/AdminConstants.cs
+++ HouseRentingSystemFromFile.Data/Data/AdminConstants.cs
@@ -5,5 +5,7 @@
 		public const string AreaName = "Admin";
 		public const string AdminRoleName = "Administrator";
 		public const string AdminEmail = "admin@mail.com";
+		public const string UsersCacheKey = "UsersCacheKey";
+		public const string RentsCacheKey = "RentsCacheKey";
 	}
 }
