d3706260f5023050dc6eda1f913cd379e7e83659
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-12 08:54:05 +0300

    Add renter foreign key in house

--- HouseRentingSystemFromFile.Data/Data/Models/House.cs
+++ HouseRentingSystemFromFile.Data/Data/Models/House.cs
@@ -4,40 +4,43 @@ using static HouseRentingSystemFromFile.Data.Data.DataConstants;
 
 namespace HouseRentingSystemFromFile.Data.Data.Models
 {
-	public class House
-	{
-		public int Id { get; set; }
+    public class House
+    {
+        public int Id { get; set; }
 
-		[Required]
-		[MaxLength(TitleMaxLength)]
-		public string Title { get; set; } = null!;
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
 
-		[Required]
-		[MaxLength(AddressMaxLength)]
-		public string Address { get; set; } = null!;
+        [Required]
+        [MaxLength(AddressMaxLength)]
+        public string Address { get; set; } = null!;
 
-		[Required]
-		[MaxLength(DescriptionMaxLength)]
-		public string Description { get; set; } = null!;
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
 
-		[Required]
-		public string ImageUrl { get; set; } = null!;
+        [Required]
+        public string ImageUrl { get; set; } = null!;
 
-		[Required]
-		public decimal PricePerMonth { get; set; }
+        [Required]
+        public decimal PricePerMonth { get; set; }
 
-		[Required]
-		public int CategoryId { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
 
-		[ForeignKey(nameof(CategoryId))]
-		public Category Category { get; set; } = null!;
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; } = null!;
 
-		[Required]
-		public int AgentId { get; set; }
+        [Required]
+        public int AgentId { get; set; }
 
-		[ForeignKey(nameof(AgentId))]
-		public Agent Agent { get; set; } = null!;
+        [ForeignKey(nameof(AgentId))]
+        public Agent Agent { get; set; } = null!;
 
-		public string? RenterId { get; set; }
-	}
+        public string? RenterId { get; set; }
+
+        [ForeignKey(nameof(RenterId))]
+        public ApplicationUser? Renter { get; set; }
+    }
 }
