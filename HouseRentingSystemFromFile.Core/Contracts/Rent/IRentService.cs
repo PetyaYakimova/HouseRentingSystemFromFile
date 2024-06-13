184f1f41fb6b5e3571f9577e77406fb69a11e523
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-13 08:26:43 +0300

    Add irent service

---
+++ HouseRentingSystemFromFile.Core/Contracts/Rent/IRentService.cs
@@ -0,0 +1,10 @@
+﻿using HouseRentingSystemFromFile.Core.Models.Rent;
+
+namespace HouseRentingSystemFromFile.Core.Contracts.Rent
+{
+    public interface IRentService
+    {
+
+        Task<IEnumerable<RentServiceModel>> All();
+    }
+}
