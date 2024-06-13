b92851cae1a5b06e68f11e6ee4045f7271f9b59f
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-13 08:26:54 +0300

    Add rent service

---
+++ HouseRentingSystemFromFile.Core/Services/Rent/RentService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using HouseRentingSystemFromFile.Core.Contracts.Rent;
+using HouseRentingSystemFromFile.Core.Models.Rent;
+using HouseRentingSystemFromFile.Data.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystemFromFile.Core.Services.Rent
+{
+    public class RentService : IRentService
+    {
+        private readonly HouseRentingDbContext _data;
+        private readonly IMapper _mapper;
+
+        public RentService(HouseRentingDbContext data, IMapper mapper)
+        {
+            _data = data;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<RentServiceModel>> All()
+        {
+            return await _data
+                .Houses
+                .Include(h => h.Agent.User)
+                .Include(h => h.Renter)
+                .Where(h => h.RenterId != null)
+                .ProjectTo<RentServiceModel>(_mapper.ConfigurationProvider)
+                .ToListAsync();
+        }
+    }
+}
