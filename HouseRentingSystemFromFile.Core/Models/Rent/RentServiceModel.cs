70a52756f19a6bea23eb85b6cd7427da31291d1c
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-13 08:27:03 +0300

    Add rent service model

---
+++ HouseRentingSystemFromFile.Core/Models/Rent/RentServiceModel.cs
@@ -0,0 +1,17 @@
+﻿namespace HouseRentingSystemFromFile.Core.Models.Rent
+{
+    public class RentServiceModel
+    {
+        public string HouseTitle { get; set; } = null!;
+
+        public string HouseImageURL { get; set; } = null!;
+
+        public string AgentFullName { get; set; } = null!;
+
+        public string AgentEmail { get; set; } = null!;
+
+        public string RenterFullName { get; set; } = null!;
+
+        public string RenterEmail { get; set;} = null!;
+    }
+}
