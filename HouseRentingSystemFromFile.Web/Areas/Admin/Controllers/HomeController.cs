c24836ecf06d8e7074cfe9920de4dfcb887f05d1
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-14 08:15:25 +0300

    Update admin home controller

---
+++ HouseRentingSystemFromFile.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystemFromFile.Web.Areas.Admin.Controllers
+{
+	public class HomeController : AdminController
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
