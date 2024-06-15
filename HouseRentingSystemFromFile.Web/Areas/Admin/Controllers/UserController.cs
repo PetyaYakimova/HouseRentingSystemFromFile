ab809531297817bf3791801252d594e384fa5759
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-15 09:43:21 +0300

    Add caching in admin user controller

--- HouseRentingSystemFromFile.Web/Areas/Admin/Controllers/UserController.cs
+++ HouseRentingSystemFromFile.Web/Areas/Admin/Controllers/UserController.cs
@@ -1,21 +1,38 @@
 ﻿using HouseRentingSystemFromFile.Core.Contracts.ApplicationUser;
+using HouseRentingSystemFromFile.Core.Models.User;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using static HouseRentingSystemFromFile.Data.Data.AdminConstants;
 
 namespace HouseRentingSystemFromFile.Web.Areas.Admin.Controllers
 {
 	public class UserController : AdminController
 	{
 		private readonly IApplicationUserService _users;
+		private readonly IMemoryCache _cache;
 
-		public UserController(IApplicationUserService users)
+		public UserController(IApplicationUserService users, IMemoryCache cache)
 		{
 			_users = users;
+			_cache = cache;
 		}
 
 		[Route("User/All")]
 		public async Task<IActionResult> All()
 		{
-			var users = await _users.All();
+			var users = _cache.Get<IEnumerable<UserServiceModel>>(UsersCacheKey);
+
+
+			if (users == null)
+			{
+				users = await _users.All();
+
+				var cacheOptions = new MemoryCacheEntryOptions()
+					.SetAbsoluteExpiration(TimeSpan.FromMinutes(5));
+
+				_cache.Set(UsersCacheKey, users, cacheOptions);
+			}
+
 			return View(users);
 		}
 	}
