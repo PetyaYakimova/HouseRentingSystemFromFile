443b704d4e10fbaef2bd32b65ecf9d748583e141
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-22 08:35:23 +0300

    Add database mock for tests

---
+++ HouseRentingSystemFromFile.Tests/Mocks/DatabaseMock.cs
@@ -0,0 +1,20 @@
+﻿using HouseRentingSystemFromFile.Data.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystemFromFile.Tests.Mocks
+{
+    public static class DatabaseMock
+    {
+        public static HouseRentingDbContext Instance
+        {
+            get
+            {
+                var dbContextOptions = new DbContextOptionsBuilder<HouseRentingDbContext>()
+                    .UseInMemoryDatabase("HouseRentingInMemoryDb" + DateTime.Now.Ticks.ToString())
+                    .Options;
+
+                return new HouseRentingDbContext(dbContextOptions, false);
+            }
+        }
+    }
+}
