f51ed3e9d4d4479ded5bc82420ba283bee1a9e21
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-22 08:35:46 +0300

    Add mapper mock for tests

---
+++ HouseRentingSystemFromFile.Tests/Mocks/MapperMock.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using HouseRentingSystemFromFile.Core.Infrastructure;
+
+namespace HouseRentingSystemFromFile.Tests.Mocks
+{
+    public static class MapperMock
+    {
+        public static IMapper Instance
+        {
+            get
+            {
+                var mapperConfiguration = new MapperConfiguration(config =>
+                {
+                    config.AddProfile<ServiceMappingProfile>();
+                });
+
+                return new Mapper(mapperConfiguration);
+            }
+        }
+    }
+}
