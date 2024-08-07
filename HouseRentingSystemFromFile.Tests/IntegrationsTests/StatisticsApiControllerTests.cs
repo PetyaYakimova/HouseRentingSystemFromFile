ca8087946f18f719435f3c35ac94fdd467504ff1
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-07 09:25:34 +0300

    Ad statistics api controller tests

---
+++ HouseRentingSystemFromFile.Tests/IntegrationsTests/StatisticsApiControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using HouseRentingSystemFromFile.Tests.Mocks;
+using HouseRentingSystemFromFile.Web.Controllers.Api;
+
+namespace HouseRentingSystemFromFile.Tests.IntegrationsTests
+{
+    [TestFixture]
+    public class StatisticsApiControllerTests
+    {
+        private StatisticApiController statisticsApiController;
+
+        [OneTimeSetUp]
+        public void SetUp() => this.statisticsApiController = new StatisticApiController(StatisticsServiceMock.Instance);
+
+        [Test]
+        public async Task GetStatistics_ShouldReturnCorrectCounts()
+        {
+            //Arrange
+
+            //Act
+            var result = await this.statisticsApiController.GetStatistic();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.That(result.TotalHouses, Is.EqualTo(10));
+            Assert.That(result.TotalRents, Is.EqualTo(6));
+        }
+    }
+}
