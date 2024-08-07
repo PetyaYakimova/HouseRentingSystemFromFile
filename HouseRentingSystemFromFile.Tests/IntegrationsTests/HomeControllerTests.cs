631e9f8fe845df9e092aadfd4078d6df0da4cd20
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-07 09:25:17 +0300

    Add home controller tests class

---
+++ HouseRentingSystemFromFile.Tests/IntegrationsTests/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using HouseRentingSystemFromFile.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystemFromFile.Tests.IntegrationsTests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        private HomeController homeController;
+
+        [OneTimeSetUp]
+        public void SetUp() => this.homeController = new HomeController(null);
+
+        [Test]
+        public void Error_ShouldReturnCorrectView()
+        {
+            //Arrange
+            var statusCode = 500;
+
+            //Act
+            var result = this.homeController.Error(statusCode);
+
+            //Assert
+            Assert.IsNotNull(result);
+            var viewResult = result as ViewResult;
+            Assert.IsNotNull(viewResult);
+        }
+    }
+}
