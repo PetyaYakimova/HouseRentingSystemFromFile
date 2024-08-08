b990bdfb5412dccbd7cd6b3ad9f5357e9ac1673a
PetyaYakimova <petya_yakimova@abv.bg>
2024-08-08 10:09:17 +0300

    Add statistics service mock

---
+++ HouseRentingSystemFromFile.Tests/Mocks/StatisticsServiceMock.cs
@@ -0,0 +1,27 @@
+﻿using HouseRentingSystemFromFile.Core.Contracts.Statistic;
+using HouseRentingSystemFromFile.Core.Models.Statistic;
+using Moq;
+
+namespace HouseRentingSystemFromFile.Tests.Mocks
+{
+    public class StatisticsServiceMock
+    {
+        public static IStatisticService Instance
+        {
+            get
+            {
+                var statisticsServiceMock = new Mock<IStatisticService>();
+
+                statisticsServiceMock
+                    .Setup(s => s.Total())
+                    .ReturnsAsync(new StatisticServiceModel()
+                    {
+                        TotalHouses = 10,
+                        TotalRents = 6
+                    });
+
+                return statisticsServiceMock.Object;
+            }
+        }
+    }
+}
